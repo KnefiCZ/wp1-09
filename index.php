5823e1fd6bf887af5083b8f43955ffa8f6d4caaf
Matěj <vas@email.com>
2018-05-17 15:19:43 +0200

    Kurva dodělána

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>kok</title>
+  </head>
+  <body>
+<h1>Holky a plat</h1>
+<?php
+    $Salary = 90000 ;
+    $Car = 1000000 ;
+?>
+<?php
+    if (($Salary >= 80000) && ($Car >= 500000) ) {
+      echo "Pravá kurva";
+    }
+    elseif ($Salary >= 80000) {
+      echo "Pražačka";
+    }
+    else {
+      echo "Hodná holka";
+    }
+ ?>
+
+
+
+  </body>
+</html>
